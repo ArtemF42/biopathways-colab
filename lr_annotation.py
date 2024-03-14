cf8d858b2300a5fe88384fd48849f2cb80808f66
dobereiner <94486570+dobereiner@users.noreply.github.com>
2024-03-14 17:07:09 +0300

    Update lr_annotation.py

--- lr_annotation.py
+++ lr_annotation.py
@@ -1,23 +1,23 @@
 import pandas as pd
+from typing import Tuple
 
-db = pd.read_csv('./biopathways/ligand_receptor/human_lr_pair.txt', sep='\t')
+LRDB = pd.read_csv('./biopathways/ligand_receptor/human_lr_pair.txt', sep='\t')
+
+LIGANDS = LRDB['ligand_gene_symbol'].unique()
+RECEPTORS = LRDB['receptor_gene_symbol'].unique()
 
 
-def annotate(df: pd.DataFrame, lr_choice: str) -> pd.DataFrame:
-    genes = df.index
-    ligands = [genes in db['ligand_gene_symbol'].unique()]
-    receptors = [genes in db['receptor_gene_symbol'].unique()]
-    
+def annotate(df: pd.DataFrame, pval: pd.DataFrame, lr_choice: str) -> Tuple[pd.DataFrame, pd.DataFrame]:    
     match lr_choice:
         case 'receptors':
-            df = df.loc[ligands]
+            df = df.loc[df.index.isin(RECEPTORS)]
         case 'ligands':
-            df = df.loc[receptors]
+            df = df.loc[df.index.isin(LIGANDS)]
         case 'both':
-            df = df.loc[set(ligands, receptors)]
+            df = df.loc[set(LIGANDS, RECEPTORS)]
             df.index = df.index.map(lambda gene: gene + ' (receptor)' 
-                                    if gene in receptors else gene + ' (ligand)')
+                                    if gene in RECEPTORS else gene + ' (ligand)')
         case _:
             raise ValueError
     
-    return df
+    return df, pval.loc[df.index]
