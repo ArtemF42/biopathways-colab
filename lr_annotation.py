5fd4659299d2ddab763cca7e836adf4c6ac358ed
dobereiner <94486570+dobereiner@users.noreply.github.com>
2024-03-14 12:58:10 +0000

    .

--- lr_annotation.py
+++ lr_annotation.py
@@ -1,5 +1,12 @@
 import pandas as pd
+# from core import Dataset
 
 db = pd.read_csv('./ligand_receptor/human_lr_pair.txt', sep='\t')
 
-print(db.head())
+
+# def annotate(df: Dataset) -> Dataset:
+#     genes = df.index.to_list()
+#     pass
+
+
+print(db)
