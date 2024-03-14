b85e4c1f5506d9dcad9988ddc576c82725b68b2f
dobereiner <94486570+dobereiner@users.noreply.github.com>
2024-03-14 13:19:03 +0000

    add annotate func

--- lr_annotation.py
+++ lr_annotation.py
@@ -1,12 +1,24 @@
 import pandas as pd
-# from core import Dataset
 
 db = pd.read_csv('./ligand_receptor/human_lr_pair.txt', sep='\t')
 
 
-# def annotate(df: Dataset) -> Dataset:
-#     genes = df.index.to_list()
-#     pass
-
-
-print(db)
+def annotate(df: pd.DataFrame, lr_choice: str) -> pd.DataFrame:
+    genes = df.index
+    ligands = [genes in db['ligand_gene_symbol'].unique()]
+    receptors = [genes in db['receptor_gene_symbol'].unique()]
+    
+    match lr_choice:
+        case 'receptors':
+            df = df.loc[ligands]
+        case 'ligands':
+            df = df.loc[receptors]
+        case 'both':
+            df = df.loc[set(ligands, receptors)]
+            df.index = df.index.map(
+                lambda gene: gene + ' (receptor)' if gene in receptors else gene + ' (ligand)'
+            )
+        case _:
+            raise ValueError
+    
+    return df
